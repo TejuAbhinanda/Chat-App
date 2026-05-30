f6d6b289a33f25341858878507d5f062feaab852
tejud <tejud@example.com>
2026-05-30 20:24:44 +0530

    Commit current workspace changes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,20 +1,15 @@
-import express from 'express';
-import {
-  signup,
-  login,
-  logout,
-  updateProfilePicture,
-  checkAuth
-} from "../controllers/auth.controller.js";
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfilePicture } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login); 
+router.post("/login", login);
 router.post("/logout", logout);
+
 router.put("/update-profile-picture", protectRoute, updateProfilePicture);
 
-router.get("/check",protectRoute,checkAuth);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
