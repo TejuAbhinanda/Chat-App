62a6753e29688b845838d216c68816c8a975d043
tejud <tejud@example.com>
2026-06-05 01:21:17 +0530

    connect frontend to render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chat-app-jf4b.onrender.com/api",
-
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
