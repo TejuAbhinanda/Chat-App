464042d55ee9081b5dcf40457f4a2f8da815ad1f
tejud <tejud@example.com>
2026-06-05 16:24:08 +0530

    fix: use relative API URL in production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chat-app-jf4b.onrender.com/api",
+      : "/api",
 
   withCredentials: true,
 });
